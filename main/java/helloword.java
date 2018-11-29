2f65d25486a4b491bacf6a6d822705508cea60b3
David Zhu <zhuleqiang@gmail.com>
2018-11-29 15:55:45 -0800

    first commit

---
+++ main/java/helloword.java
@@ -0,0 +1,16 @@
+public class helloword {
+
+    public static  void  main(String[] args){
+        int a=10;
+        int b=11;
+
+        int [] nums={1,2,4,69,7,8};
+
+        System.out.println("hello SB");
+        System.out.println(a+b);
+        System.out.println(nums[3]);
+
+
+
+    }
+}
